07215bc600208b3cec070049aab00afe8c8799ea
Aldulea Cristi <alduleacristi@yahoo.com>
2015-03-07 16:08:38 +0200

    Modified client

--- GroupManager/GroupManagerWeb/groupmanager-common/src/main/java/org/groupmanager/team/responses/GroupManagerResponse.java
+++ GroupManager/GroupManagerWeb/groupmanager-common/src/main/java/org/groupmanager/team/responses/GroupManagerResponse.java
@@ -1,28 +1,34 @@
 package org.groupmanager.team.responses;
 
-public class Response {
+import org.groupmanager.team.common.ErrorList;
+
+public class GroupManagerResponse {
 	private String message;
 	private String errorMessage;
-	private String httpStatusCode;
-	
+	private ErrorList error;
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
+
 	public String getErrorMessage() {
 		return errorMessage;
 	}
+
 	public void setErrorMessage(String errorMessage) {
 		this.errorMessage = errorMessage;
 	}
-	public String getHttpStatusCode() {
-		return httpStatusCode;
+
+	public ErrorList getError() {
+		return error;
 	}
-	public void setHttpStatusCode(String httpStatusCode) {
-		this.httpStatusCode = httpStatusCode;
+
+	public void setError(ErrorList error) {
+		this.error = error;
 	}
-	
-	
+
 }
