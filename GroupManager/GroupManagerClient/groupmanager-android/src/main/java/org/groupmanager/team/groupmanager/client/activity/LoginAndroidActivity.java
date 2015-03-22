e10d2f8203cad3feefd6d973aeb10ff0ce5034f7
Aldulea Cristi <alduleacristi@yahoo.com>
2015-03-22 12:23:22 +0200

    changed activity_main to activity_login in LoginAndroidActivity.java

--- GroupManager/GroupManagerClient/groupmanager-android/src/main/java/org/groupmanager/team/groupmanager/client/activity/LoginAndroidActivity.java
+++ GroupManager/GroupManagerClient/groupmanager-android/src/main/java/org/groupmanager/team/groupmanager/client/activity/LoginAndroidActivity.java
@@ -40,7 +40,7 @@ public class LoginAndroidActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_login);
 
 		final EditText email = (EditText) findViewById(R.id.email);
 		final EditText password = (EditText) findViewById(R.id.password);
