3cb75b7da59cdb5c666b61aaed8b284ee93f4437
Bogdan <bogdan.fieraru22@gmail.com>
2015-05-06 22:39:35 +0300

    update autofill

--- GroupManager/GroupManagerClient/GroupManagerAndroid/app/src/main/java/team/groupmanager/org/groupmanager/UpdateProfileActivity.java
+++ GroupManager/GroupManagerClient/GroupManagerAndroid/app/src/main/java/team/groupmanager/org/groupmanager/UpdateProfileActivity.java
@@ -40,54 +40,93 @@ public class UpdateProfileActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_update_profile);
-        showMessageUtil = new ShowMessageUtil(handler,UpdateProfileActivity.this);
+        showMessageUtil = new ShowMessageUtil(handler, UpdateProfileActivity.this);
         sharedPreferencesUtil = new SharedPreferencesUtil(UpdateProfileActivity.this);
 
         final String email = sharedPreferencesUtil.getEmail();
-        //String password = sharedPreferencesUtil.getPw();
-        //String lastName = sharedPreferencesUtil.getLastName();
-        //String firstName = sharedPreferencesUtil.getFirstName();
         TextView helloUser = (TextView) findViewById(R.id.helloUser);
         helloUser.setText(email);
 
-        final ImageButton updateProfile = (ImageButton) findViewById(R.id.UpdateProfile);
-        updateProfile.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0){
-                EditText firstNameET = (EditText)findViewById(R.id.textFirstName);
-                EditText lastNameET = (EditText)findViewById(R.id.textLastName);
-                EditText passwordET = (EditText)findViewById(R.id.new_password);
-                EditText retypedPasswordET = (EditText)findViewById(R.id.new_retyped_Password);
-                EditText emailET = (EditText)findViewById(R.id.new_Email);
-                UserDTO userDTO = null;
-                List<UserDTO> users = null;
-                try{
-                UserCommunication userCommunication = new UserCommunication();
-                users = userCommunication.getUsersByEmail(email, Constants.URL+ "/GroupManager/api/security/users/getUsers", "token");}
-                catch (GroupManagerClientException e) {
+        final EditText firstNameET = (EditText) findViewById(R.id.textFirstName);
+        final EditText lastNameET = (EditText) findViewById(R.id.textLastName);
+        final EditText passwordET = (EditText) findViewById(R.id.new_password);
+        final EditText retypedPasswordET = (EditText) findViewById(R.id.new_retyped_Password);
+        final EditText emailET = (EditText) findViewById(R.id.new_Email);
+
+        Runnable runnableComplete = new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    UserCommunication userCommunication = new UserCommunication();
+                    final List<UserDTO> users = userCommunication.getUsersByEmail(email, Constants.URL + "/GroupManager/api/security/users/getUsers", sharedPreferencesUtil.getToken());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                         UserDTO userDTO = null;
+                         if (users.size() > 0) {
+                            userDTO = users.get(0);
+                            firstNameET.setText(userDTO.getFirstName());
+                            lastNameET.setText(userDTO.getLastName());
+                            emailET.setText(userDTO.getEmail());
+                            emailET.setFocusable(false);
+                            emailET.setClickable(false);
+                            }
+                        }
+                    });
+                } catch (GroupManagerClientException e) {
                     showMessageUtil.showToast("Failed to get users.", Toast.LENGTH_SHORT);
                 }
-                if(users.size()>0)
-                    userDTO = users.get(0);
-                if(firstNameET.getText().toString()!="")
-                    userDTO.setFirstName(firstNameET.getText().toString());
-                if(lastNameET.getText().toString()!="")
-                    userDTO.setLastName(lastNameET.getText().toString());
-                if(emailET.getText().toString()!="")
-                    userDTO.setEmail(emailET.getText().toString());
-                if(passwordET.getText().toString()!="" && passwordET.getText().toString() == retypedPasswordET.getText().toString())
-                    userDTO.setPassword(passwordET.getText().toString());
-                try{
-                AccountCommunications acc = new AccountCommunications();
-                GroupManagerResponse resp = acc.updateUser(userDTO,
-                        Constants.URL+"/GroupManager/api/security/users/updateUser","f865eab2-c1b7-427d-9287-998453793001");
+            }};
 
-                showMessageUtil.showToast("Profile updated!", Toast.LENGTH_SHORT);
-                //System.out.println(resp.getMessage());
-                //System.out.println(resp.getErrorMessage());
-                }
-                catch (GroupManagerClientException e) {
-                showMessageUtil.showToast("Failed to update profile", Toast.LENGTH_SHORT);
-                }
+        Thread threadComplete = new Thread(runnableComplete);
+        threadComplete.start();
+
+        final ImageButton updateProfile = (ImageButton) findViewById(R.id.UpdateProfile);
+        updateProfile.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                Runnable runnable = new Runnable() {
+                        @Override
+                    public void run() {
+                        UserDTO userDTO = null;
+                        List<UserDTO> users = null;
+
+                        try {
+                            UserCommunication userCommunication = new UserCommunication();
+                            users = userCommunication.getUsersByEmail(email, Constants.URL + "/GroupManager/api/security/users/getUsers", sharedPreferencesUtil.getToken());
+                        } catch (GroupManagerClientException e) {
+                            showMessageUtil.showToast("Failed to get users.", Toast.LENGTH_SHORT);
+                        }
+                        if (users.size() > 0)
+                            userDTO = users.get(0);
+                        if (!firstNameET.getText().toString().equals(""))
+                            userDTO.setFirstName(firstNameET.getText().toString());
+                        if (!lastNameET.getText().toString().equals(""))
+                            userDTO.setLastName(lastNameET.getText().toString());
+                        if (!emailET.getText().toString().equals(""))
+                            userDTO.setEmail(emailET.getText().toString());
+                        if ((passwordET.getText().toString().equals("")==false) && passwordET.getText().toString().equals(retypedPasswordET.getText().toString()))
+                            userDTO.setPassword(passwordET.getText().toString());
+                        try {
+                            AccountCommunications acc = new AccountCommunications();
+                            GroupManagerResponse resp = acc.updateUser(userDTO,
+                                    Constants.URL + "/GroupManager/api/security/users/updateUser", sharedPreferencesUtil.getToken());
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    showMessageUtil.showToast("Profile updated!", Toast.LENGTH_SHORT);
+                                }
+                            });
+
+                        } catch (GroupManagerClientException e) {
+                            showMessageUtil.showToast("Failed to update profile", Toast.LENGTH_SHORT);
+                        }
+                    }
+                };
+                Thread thread = new Thread(runnable);
+                thread.start();
             }
         });
     }
